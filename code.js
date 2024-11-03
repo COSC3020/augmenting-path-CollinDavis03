9d1f676c606ca4bde22b4e3c28e2744b1de7b306
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-11-03 16:20:03 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,46 @@
 function augmentingPath(graph, start, end) {
+    //Handle where start and end nodes are the same 
+    if (start === end) {
+        return [start]; 
+    }
+    
+    //Queue DFS and a mao to keep track of paths 
+    let queue = [start];
+    let visited = new set(); 
+    let parenMap = {}; 
+
+    //Start DFS 
+    while (queue > 0){
+        let current = queue.shift();
+
+        //mark current node visited 
+        visited.add(current);
+        
+        //Iterate over the neighbors of the current nodes 
+        for (let neighbor in graph[current]) {
+            //Check if the neighbor is not visited 
+            if (!visited.has(neighbor) && graph[current][neighbor] > 0) {
+                //Record the path 
+                parenMap[neighor] = current; 
+
+                 // Reach end build path and return 
+                if (neighbor === end) {
+                    let path = [end]; 
+                    let step = end; 
+
+                    while (step !== start) { 
+                        step = parenMap[step];
+                        path.unshift(step);
+                    }
+                    return path; 
+                }
+                // add the neighbor to queue
+                queue.push(neighbor);
+                visited.add(neighbor);
+            }
+        }
+    }
+
+    //if not path found return it
     return [];
 }
